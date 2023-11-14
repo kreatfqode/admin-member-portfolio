b711b255a65c0ab395b5da240254f2dbd299c6d0
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-11-14 14:07:45 +0700

    feat: vuetify

---
+++ src/@core/components/cards/CardStatisticsVertical.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  stats: {
+    type: String,
+    required: true,
+  },
+})
+
+</script>
+
+<template>
+  <VCard>
+    <VCardText class="d-flex align-center pb-4">
+      <img width="42" :src="props.image" alt="image">
+    </VCardText>
+
+    <VCardText>
+      <b class="mb-1">
+        {{ props.title }}
+      </b>
+      <h4 class="text-h4 text-no-wrap">
+        {{ props.stats }}
+      </h4>
+    </VCardText>
+  </VCard>
+</template>
---
+++ src/@core/config.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    apiTarget: "http://localhost:8000",
+    deploymenBase: "admin/",
+    imagePath: "public/images/upload"
+}
+// http://localhost:8000
+// http://cubepro.000webhostapp.com
+</script>
